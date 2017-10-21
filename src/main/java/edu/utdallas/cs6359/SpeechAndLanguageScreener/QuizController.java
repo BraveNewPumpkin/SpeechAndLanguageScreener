e009649d141b87f89feba073e46b5c775eedee31
kdbolton <latestthing@gmail.com>
2017-10-20 22:22:26 -0500

    added section abstraction

--- src/main/java/edu/utdallas/cs6359/SpeechAndLanguageScreener/QuizController.java
+++ src/main/java/edu/utdallas/cs6359/SpeechAndLanguageScreener/QuizController.java
@@ -11,11 +11,14 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class QuizController {
-
     @RequestMapping(value = "/quiz", method = RequestMethod.GET)
     public String renderQuizView(
-            @RequestParam(value="question_template", required=false, defaultValue="question_1") String question_template,
+            @RequestParam(value="section_template", required=false, defaultValue="Section_1") String section_template,
+            @RequestParam(value="question_template", required=false, defaultValue="Section_1/Question_1") String question_template,
             Model model) {
+        String user_name = "Stu Dent"; //TODO implement current user name
+        model.addAttribute("user_name", user_name);
+        model.addAttribute("section_template", section_template);
         model.addAttribute("question_template", question_template);
         return "QuizPage";
     }
--- src/main/java/edu/utdallas/cs6359/SpeechAndLanguageScreener/SurveySection.java
+++ src/main/java/edu/utdallas/cs6359/SpeechAndLanguageScreener/SurveySection.java
@@ -1,6 +1,13 @@
 package edu.utdallas.cs6359.SpeechAndLanguageScreener;
 
+import java.util.LinkedList;
+
 public class SurveySection extends Section{
+
+    public SurveySection(String quiz_template_path, String score_template_path, LinkedList<Question> questions){
+        super(quiz_template_path, score_template_path, questions);
+    }
+
     @Override
     public Score calcScore() {
         //TODO implement
